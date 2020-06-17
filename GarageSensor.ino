61469eb4d34ca2232d4bbd9d4fb6bbfc5f05e487
Deo Garage <dar8900@gmail.com>
2020-06-17 23:14:40 +0200

    -Inserite scritte sul display per visualizzare lo stato

--- GarageSensor.ino
+++ GarageSensor.ino
@@ -19,7 +19,14 @@ static void ReadTempHumidity()
 
 static String FormatTime(uint32_t time)
 {
-
+	String timeStr = "";
+	timeStr = (time / 86400) > 10 ? String(time / 86400) : "0" + String(time / 86400);
+	timeStr += "d";
+	timeStr += ((time / 3600) / 24) > 10 ? String(((time / 3600) / 24)) : "0" + String(((time / 3600) / 24));
+	timeStr += "h";
+	timeStr += ((time / 60)  % 60) > 10 ? String(((time / 60)  % 60)) : "0" + String(((time / 60)  % 60));
+	timeStr += "s";
+	return timeStr;
 }
 
 void setup()
@@ -30,14 +37,26 @@ void setup()
 	pinMode(BUTTON_PIN, INPUT);
 	Lcd.begin();
 	TempSensor.begin();
-	Ligh.function = AUTO_MODE;
-	Ligh.moveDetected = false;
+	Light.function = AUTO_MODE;
+	Light.moveDetected = false;
 	Light.status = OFF;
 }
 
 
 void loop()
 {
-	String LastLogin = "Last log: "
-	Lcd.printString(ONE, CENTER_ALIGN, )
+	String StringToStamp = "Last log: " + FormatTime(LastLoginTimeStamp);
+	Lcd.printString(ONE, CENTER_ALIGN, StringToStamp.c_str());
+	StringToStamp = String(TempVar.temperature, 1) + "C" + "  " + String(TempVar.humidity, 1) + "%";
+	Lcd.printString(TWO, CENTER_ALIGN, StringToStamp.c_str());
+	if(Light.function == AUTO_MODE)
+		StringToStamp = "PIR mode: AUTO";
+	else
+		StringToStamp = "PIR mode: MANUAL";
+	Lcd.printString(THREE, CENTER_ALIGN, StringToStamp.c_str());
+	if(Light.function == AUTO_MODE && Light.status == ON)
+	{
+		// Stampare il tempo rimanente per lo spegnimento della luce
+		// Lcd.printString(FOUR, CENTER_ALIGN, LastLogin.c_str());
+	}
 }
